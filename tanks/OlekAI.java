c92f64e0e850d64c0c9ee86948bb9cbe7d57b9bc
KolrowyAleksander <ollo17sz@gmail.com>
2016-01-18 22:46:22 +0100

    A

--- tanks/OlekAI.java
+++ tanks/OlekAI.java
@@ -1,28 +1,20 @@
 package tanks;
 
-import java.util.List;
-
 public class OlekAI extends ArtificialPlayer {
-
-    private Move.Movement currentMovement;
-    private Move.Rotation currentRotation;
 
     private double myX;
     private double myY;
     private double enemyX;
     private double enemyY;
 
-    public OlekAI(int playerNumber, String playerName, Tank tank) {
-        super(playerNumber, playerName, tank);
-    public OlekAI (int playerNumber, Tank tank) {
-        super(playerNumber, "Olek222", tank);
+    public OlekAI(int playerNumber, Tank tank) {
+        super(playerNumber, "BAWS", tank);
     }
 
     private boolean seeTank() {
         for (RoundGameObject element : visibleObjectsBuffer) {
             if (element instanceof Tank) {
                 enemyX = element.getCenterX();
-                enemyY = element.getCenterY();
                 enemyY = element.getCenterY();
                 return true;
             }
@@ -33,11 +25,11 @@ public class OlekAI extends ArtificialPlayer {
     private double countAlpha(double enemyX, double enemyY) {
         double alpha = Math.toDegrees(Math.atan2(enemyY - myY, enemyX - myX));
         alpha -= 180;
-            if (alpha < 0) {
-                alpha += 360;
-            }
-            return alpha;
+        if (alpha < 0) {
+            alpha += 360;
         }
+        return alpha;
+    }
 
     private Move.Rotation rotate(double alpha) {
         Move.Rotation rotation = Move.Rotation.Staying;
@@ -52,14 +44,13 @@ public class OlekAI extends ArtificialPlayer {
         double alpha;
         myX = this.getPlayerTank().getCenterX();
         myY = this.getPlayerTank().getCenterY();
+        Move.Rotation currentRotation;
         if (seeTank()) {
             alpha = countAlpha(enemyX, enemyY);
-            //System.out.println(alpha);
             currentRotation = rotate(alpha);
         } else {
             currentRotation = Move.Rotation.CounterClockwise;
         }
-        currentMovement = Move.Movement.Staying;
-        return new Move(currentMovement, currentRotation, Move.Shooting.Shoots);
+        return new Move(Move.Movement.Forward, currentRotation, Move.Shooting.Shoots);
     }
 }
