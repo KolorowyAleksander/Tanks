b15cabb5530e05f1f830d355350800eced322438
KolrowyAleksander <ollo17sz@gmail.com>
2016-01-25 08:10:43 +0100

    A

--- tanks/OlekAI.java
+++ tanks/OlekAI.java
@@ -2,10 +2,7 @@ package tanks;
 
 public class OlekAI extends ArtificialPlayer {
 
-    private double myX;
-    private double myY;
-    private double enemyX;
-    private double enemyY;
+    private Tank enemy;
 
     public OlekAI(int playerNumber, Tank tank) {
         super(playerNumber, "BAWS", tank);
@@ -14,40 +11,41 @@ public class OlekAI extends ArtificialPlayer {
     private boolean seeTank() {
         for (RoundGameObject element : visibleObjectsBuffer) {
             if (element instanceof Tank) {
-                enemyX = element.getCenterX();
-                enemyY = element.getCenterY();
+                enemy = (Tank) element;
                 return true;
             }
         }
         return false;
     }
 
-    private double countAlpha(double enemyX, double enemyY) {
-        double alpha = Math.toDegrees(Math.atan2(enemyY - myY, enemyX - myX));
-        alpha -= 180;
-        if (alpha < 0) {
-            alpha += 360;
-        }
+    private double countAlpha() {
+        double alpha = Math.toDegrees(Math.atan2(enemy.getCenterY() - this.getPlayerTank().getCenterY(),
+                enemy.getCenterX() - this.getPlayerTank().getCenterY()));
+
+        alpha += 450;
+
+        alpha = alpha % 360;
+
         return alpha;
     }
 
-    private Move.Rotation rotate(double alpha) {
+    private Move.Rotation setRotation(double alpha) {
         Move.Rotation rotation = Move.Rotation.Staying;
-        if (alpha >= this.getPlayerTank().getRotationAngle())
+        double beta = this.getPlayerTank().getRotationAngle();
+        if (alpha > beta)
             rotation = Move.Rotation.CounterClockwise;
-        if (alpha < this.getPlayerTank().getRotationAngle())
+        else if (alpha < beta)
             rotation = Move.Rotation.Clockwise;
         return rotation;
     }
 
+    private Move.Rotation currentRotation;
+
     public Move makeMove(double deltaTime) {
         double alpha;
-        myX = this.getPlayerTank().getCenterX();
-        myY = this.getPlayerTank().getCenterY();
-        Move.Rotation currentRotation;
         if (seeTank()) {
-            alpha = countAlpha(enemyX, enemyY);
-            currentRotation = rotate(alpha);
+            alpha = countAlpha();
+            currentRotation = setRotation(alpha);
         } else {
             currentRotation = Move.Rotation.CounterClockwise;
         }
